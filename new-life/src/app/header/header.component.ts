2bc2a6c8002980e1d3a5a8384866509b1b0d72c8
Adrian Bleoju <bleojua98@gmail.com>
2024-06-18 13:12:03 +0300

    updates

--- new-life/src/app/header/header.component.ts
+++ new-life/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-header',
@@ -6,5 +8,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  constructor(private meta: Meta) {
+    this.meta.addTag({ name: 'description', content: 'Centrul Internațional de Psihologie și Psihoterapie NEW LIFE THERAPY, examinari psihologice pentru obtinere permis auto,control periodic, angajare, port arma letala si neletala, profil personalitate, dosar medical, psihodiagnostic, psihoterapeut pentru adulti, cupluri, adolescenti si copii. - Miercurea Ciuc - Costoiu Sándor / Costoiu Sándor - Csikszereda - NEW LIFE THERAPY Pszihológiai és Pszihoterápiai Nemzetközi Központ HAJTÁSI ENGEDÉLYHEZ, PERIÓDIKUS VIZSGÁLATOK, ALKALMAZÁSHOZ, FEGYVERTARTÁSI ENGEDÉLYHEZ, SZEMÉLYSÉG PROFIL, ORVOSI DOSSZIÉHOZ, PSZIHOLÓGIAI DIAGNÓZIS PSZIHOLÓGIAI VIZSGÁLATOK Pszichológiai tanácsadás gyerekek, tinédzserek, felnőttek és párok részére' });
+    this.meta.addTag({ property: 'og:title', content: 'Titlul Open Graph' });
+    // Adaugă orice alte meta-tag-uri de care ai nevoie
+  }
 }
