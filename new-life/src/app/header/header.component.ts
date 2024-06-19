cb7e6c2ad3f4228deb39c6c9041e117eee41724a
Adrian Bleoju <bleojua98@gmail.com>
2024-06-19 14:55:08 +0300

    update meta tags

--- new-life/src/app/header/header.component.ts
+++ new-life/src/app/header/header.component.ts
@@ -8,9 +8,17 @@ import { Meta } from '@angular/platform-browser';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  constructor(private meta: Meta) {
-    this.meta.addTag({ name: 'description', content: 'Centrul Internațional de Psihologie și Psihoterapie NEW LIFE THERAPY, examinari psihologice pentru obtinere permis auto,control periodic, angajare, port arma letala si neletala, profil personalitate, dosar medical, psihodiagnostic, psihoterapeut pentru adulti, cupluri, adolescenti si copii. - Miercurea Ciuc - Costoiu Sándor / Costoiu Sándor - Csikszereda - NEW LIFE THERAPY Pszihológiai és Pszihoterápiai Nemzetközi Központ HAJTÁSI ENGEDÉLYHEZ, PERIÓDIKUS VIZSGÁLATOK, ALKALMAZÁSHOZ, FEGYVERTARTÁSI ENGEDÉLYHEZ, SZEMÉLYSÉG PROFIL, ORVOSI DOSSZIÉHOZ, PSZIHOLÓGIAI DIAGNÓZIS PSZIHOLÓGIAI VIZSGÁLATOK Pszichológiai tanácsadás gyerekek, tinédzserek, felnőttek és párok részére' });
-    this.meta.addTag({ property: 'og:title', content: 'Titlul Open Graph' });
-    // Adaugă orice alte meta-tag-uri de care ai nevoie
+  constructor(private meta: Meta) {}
+  ngOnInit() {
+    // Meta tag-uri pentru SEO
+    this.meta.addTags([
+      { name: 'description', content: 'Centrul Internațional de Psihologie și Psihoterapie NEW LIFE THERAPY, examinari psihologice pentru obtinere permis auto, control periodic, angajare, port arma letala si neletala, profil personalitate, dosar medical, psihodiagnostic, psihoterapeut pentru adulti, cupluri, adolescenti si copii. NEW LIFE THERAPY Pszihológiai és Pszihoterápiai Nemzetközi Központ, pszichológiai vizsgálatok vezetői engedély megszerzéséhez, időszakos vizsgálatok, alkalmazáshoz, halálos és nem halálos fegyver viseléséhez, személyiség profil, orvosi dosszié, pszichodiagnosztika, pszichoterapeuta felnőttek, párok, serdülők és gyermekek számára.' },
+      { name: 'keywords', content: 'psihologie, psihoterapie, terapie, Miercurea Ciuc, Harghita, sănătate mentală, consiliere, terapie de cuplu, terapie de grup, pszichológia, pszichoterápia, terápia, Csíkszereda, Hargita, mentális egészség, tanácsadás, párterápia, csoportterápia' },
+      { property: 'og:type', content: 'website' },
+      { property: 'og:url', content: 'https://www.site-tau.ro/' },
+      { property: 'og:title', content: 'New Life Therapy - Cabinet de Psihologie și Psihoterapie în Miercurea Ciuc / Pszichológiai és Pszichoterápiai Központ Csíkszeredában' },
+      { property: 'og:description', content: 'Cabinet de Psihologie și Psihoterapie în Miercurea Ciuc, oferind terapie individuală, de cuplu și de grup. Pszichológiai és pszichoterápiai központ Csíkszeredában, egyéni, pár- és csoportterápia.' },
+      { property: 'og:image', content: 'https://www.site-tau.ro/assets/img/logo-new-life.jpg' }
+    ]);
   }
 }
